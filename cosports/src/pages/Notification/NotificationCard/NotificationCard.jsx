99e3a5c201e5d13b4eff4b592570b4b4fdc8be25
Jay Rajput <74800982+jay-rajput30@users.noreply.github.com>
2022-02-25 17:31:08 +0530

    fix: bug fixes

--- cosports/src/pages/Notification/NotificationCard/NotificationCard.jsx
+++ cosports/src/pages/Notification/NotificationCard/NotificationCard.jsx
@@ -1,8 +1,28 @@
 import "./NotificationCard.css";
+import { getDayDifference } from "../../../utils/cardUtils";
+import { useNavigate } from "react-router-dom";
 
 const NotificationCard = ({ notification }) => {
-  console.log({ text: "inside card", notification });
-  return <p>{`${notification.username}${notification.actionString}`}</p>;
+  const navigate = useNavigate();
+  console.log({
+    text: "inside card",
+    date: new Date(
+      new Date().getTime() - new Date(notification.createdAt).getTime()
+    ).getDate(),
+  });
+
+  const cardClickHandler = (postId) => {
+    navigate(`/singlepost/${postId}`);
+  };
+  return (
+    <div
+      className="notification--card--container"
+      onClick={() => cardClickHandler(notification.postId)}
+    >
+      <p className="notification">{`${notification.username}${notification.actionString}`}</p>
+      <span>{getDayDifference(notification.createdAt)}</span>
+    </div>
+  );
 };
 
 export default NotificationCard;
--- cosports/src/utils/cardUtils.js
+++ cosports/src/utils/cardUtils.js
@@ -13,3 +13,24 @@ export const getUserInitials = (firstName, lastName = "") => {
   const last = lastName.length !== 0 ? lastName.split("")[0].toUpperCase() : "";
   return first + last;
 };
+
+export const getDayDifference = (date) => {
+  console.log({ date });
+
+  const diff = new Date().getTime() - new Date(date).getTime;
+
+  let days = new Date(
+    new Date().getTime() - new Date(date).getTime()
+  ).getDate();
+
+  if (days > 60) {
+    return "few months ago";
+  }
+  if (days > 30) {
+    return "a month ago";
+  }
+  if (days >= 7) {
+    return "a week ago";
+  }
+  return `${days} day ago`;
+};
