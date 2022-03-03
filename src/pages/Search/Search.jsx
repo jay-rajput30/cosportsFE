0a9947df37f4ea1ff84277617397519fb23b8fa8
Jay Rajput <74800982+jay-rajput30@users.noreply.github.com>
2022-03-03 17:30:33 +0530

    refactor: changed the onChange code for Search input page

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -11,8 +11,8 @@ import UserCard from "./UserCard/UserCard";
 const Search = ({ showModal, setShowModal }) => {
   const dispatch = useDispatch();
   const user = useSelector((state) => state.user);
-  const [showComponent, setShowComponent] = useState(false);
-  const [setUserSearched] = useState();
+  // const [showComponent, setShowComponent] = useState(false);
+  // const [setUserSearched] = useState();
   const userFound = useSelector((state) => state.users);
 
   console.log({ userFound: userFound });
@@ -20,27 +20,19 @@ const Search = ({ showModal, setShowModal }) => {
   useEffect(() => {
     const fetchUsers = async () => {
       dispatch(getAllUsers(user.token));
-      // setUserFound(users.searchedAccounts);
     };
 
     fetchUsers();
   }, []);
 
-  //when search term is entered make backend call to fetch the account details and show user details on card.
-  //on card click view the profile of the user on a new page
-  // onclick of the follow button on this view pofile page the followers count should update on screen
-  //and the logged in user's following count should also update.
-  //also the text of follow button should change to follow/unfollow on click.d
-
   return (
     <div className="search--container">
       <SearchInput
-        setUserSearched={setUserSearched}
+        // setUserSearched={setUserSearched}
         showModal={showModal}
         setShowModal={setShowModal}
-        showComponent={showComponent}
-        setShowComponent={setShowComponent}
-        // setUserFound={setUserFound}
+        // showComponent={showComponent}
+        // setShowComponent={setShowComponent}
       />
       <article className="user--found--container">
         {userFound.searchedAccounts.length === 0 ? (
@@ -51,7 +43,7 @@ const Search = ({ showModal, setShowModal }) => {
           })
         )}
       </article>
-      {/* if( userFound.length > 0){} */}
+
       <NavbarMobile showModal={showModal} setShowModal={setShowModal} />
       <NavbarDesktop showModal={showModal} setShowModal={setShowModal} />
       {showModal.status && (
@@ -61,23 +53,23 @@ const Search = ({ showModal, setShowModal }) => {
   );
 };
 const SearchInput = ({
-  setUserSearched,
+  // setUserSearched,
   showModal,
   setShowModal,
-  showComponent,
-  setShowComponent,
+  // showComponent,
+  // setShowComponent,
   setUserFound,
 }) => {
   const dispatch = useDispatch();
   const [searchTerm, setSearchTerm] = useState("");
   const user = useSelector((state) => state.user);
   const searchKeyPressHandler = (e) => {
-    // setSearchTerm((prev) => prev + e.key);
-    setUserSearched((prev) => searchTerm);
+    setSearchTerm(e.target.value);
+    // setUserSearched(searchTerm);
 
     // if (e.key === "Enter") {
     console.log(searchTerm);
-    setShowComponent(true);
+    // setShowComponent(true);
     // setSearchTerm((prev) => "");
     dispatch(getAccountDetail({ searchTerm, token: user.token }));
     // }
@@ -89,8 +81,8 @@ const SearchInput = ({
         className="search--input"
         placeholder="type username and hit enter"
         value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-        onKeyPress={(e) => searchKeyPressHandler(e)}
+        // onChange={(e) => setSearchTerm(e.target.value)}
+        onChange={(e) => searchKeyPressHandler(e)}
       />
       <NavbarMobile showModal={showModal} setShowModal={setShowModal} />
       <NavbarDesktop showModal={showModal} setShowModal={setShowModal} />
